3467fc04c97080d250c39e310b17c79fbe83691e
diyanamendoza <diyana.amp@pm.me>
2021-08-31 21:11:14 -0700

    Add poke render, getSeenCounts, updateSeenCounts functions

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-// import functions and grab DOM elements
+import { getRandomPokemon, updateSeenCounts } from "./utils.js";
+import pokemonArray from "./pokemon.js";
 
-// initialize global state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+
+let pokeLineUp = getRandomPokemon();
+
+function renderPoke(poke) {
+  const pokeContainer = document.getElementById('poke-container');
+  const pokeLabel = document.createElement('label');
+  const pokeImg = document.createElement('img');
+  const pokeInput = document.createElement('input');
+
+  pokeLabel.classList.add('pokemon');
+  pokeImg.src = poke.url_image;
+  pokeInput.value = poke.id;
+  console.log(poke.id);
+  pokeInput.type = 'radio';
+  pokeInput.setAttribute('name', 'pokemon');
+
+  pokeContainer.append(pokeLabel);
+  pokeLabel.append(pokeInput, pokeImg);
+
+  pokeLabel.addEventListener('click', () => {
+    console.log(`you caught ${poke.pokemon}!`);
+  })
+
+};
+
+for(let poke of pokeLineUp) {
+  renderPoke(poke);
+  updateSeenCounts(poke);
+};
--- utils.js
+++ utils.js
@@ -1,4 +1,67 @@
 //imports
-import pokemon from "./pokemon.js";
+import pokemonArray from "./pokemon.js";
 
 //export functions
+
+function getRandomIndex() {
+    return Math.floor(Math.random() * pokemonArray.length);
+}
+
+export function getRandomPokemon() {
+    let randomIndex1 = getRandomIndex();
+    let randomIndex2 = getRandomIndex();
+    let randomIndex3 = getRandomIndex();
+
+    while (
+        randomIndex1 === randomIndex2 
+        || randomIndex2 === randomIndex3 
+        || randomIndex1 === randomIndex3) {
+        randomIndex1 = getRandomIndex();
+        randomIndex2 = getRandomIndex();
+        randomIndex3 = getRandomIndex();
+    }
+
+    return [
+        pokemonArray[randomIndex1], 
+        pokemonArray[randomIndex2], 
+        pokemonArray[randomIndex3]
+    ];
+}
+export function getSeenCounts(pokemonArray) {
+    // pull from local storage
+    let seenCounts = localStorage.getItem('SEEN');
+    // if nothing in there yet, create area of poke objects
+    if (!seenCounts) {
+      seenCounts = pokemonArray.map(entry => {
+        let properties = {
+          'name': entry.pokemon,
+          'seen': 0
+        }
+        return properties;
+      });
+      console.log(seenCounts);
+      let stringSeenCounts = JSON.stringify(seenCounts);
+      localStorage.setItem('SEEN', stringSeenCounts);
+    }
+    // if something in there, 
+    const parsedSEEN = JSON.parse(seenCounts);
+    return parsedSEEN;
+  };
+
+export function updateSeenCounts(poke) {
+    let currentCounts = getSeenCounts(pokemonArray);
+    let pokeName = poke.pokemon;
+  
+    let update = currentCounts.map(entry => {
+      if(entry.name === pokeName) {
+        entry.seen += 1;
+        return entry;
+      } else {
+        return entry;
+      }
+    })
+  
+    let stringUpdate = JSON.stringify(update);
+    localStorage.setItem('SEEN', stringUpdate);
+  };
+  
